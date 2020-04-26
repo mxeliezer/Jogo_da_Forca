97fea487d61aff19eda3bbc8bb0254ae4b28aeb3
Eliezer <57819235+mxeliezer@users.noreply.github.com>
2020-04-26 21:08:18 +0100

    Upload programa do jogo
    Upload programa do jogo

--- forca.c
+++ forca.c
@@ -10,14 +10,52 @@ Conhecendo um pouco mais sobre funções e parâmetros e ponteiros.
 Ponteiros e operações com arrays.
 */
 
-// Variáveis globais:
+// 🚩️ Variáveis globais:
 
-char palavra_secreta[20];
+char palavra_secreta[TAMANHO_PALAVRA];
 char chutes[26];
 int chutes_dados = 0;
 
 
-// Funções de controle:
+// 🚩️ Funções de controle:
+
+int letra_existe(char letra) {
+    for(int j = 0; j < strlen(palavra_secreta); j++) {
+        if(letra == palavra_secreta[j]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// Definindo o desfecho do jogo: venceu ou perdeu?
+
+int chutes_errados() {
+	int erros = 0;
+	for(int i = 0; i < chutes_dados; i++) {
+		if(!letra_existe(chutes[i])) {
+			erros++;
+		}
+	}
+	return erros;
+}
+
+// 😭️
+int enforcou() {
+	return chutes_errados() >= 5;
+}
+
+// 😄️
+int acertou(){
+	for(int i = 0; i < strlen(palavra_secreta); i++) {
+		if(!ja_chutou(palavra_secreta[i])){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+// Cabeçalho do jogo:
 
 void abertura(){
 	printf(" ************************************************************************   \n");
@@ -33,35 +71,52 @@ void abertura(){
 	printf(" ************************************************************************   \n");
 }
 
-void chuta(){ 
+void chuta(){
 	char chute;
 	printf("Chuta uma letra!\n");
 	scanf(" %c", &chute);
+	if(letra_existe(chute)) {
+        printf("Você acertou: a palavra tem a letra %c\n\n", chute);
+    } else {
+        printf("\nVocê errou: a palavra NÃO tem a letra %c\n\n", chute);
+    }
 	chutes[chutes_dados] = chute;
 	chutes_dados++;
 }
 
-int ja_chutou (char letra){ 
+int ja_chutou (char letra){
 	int achou = 0;
 	for(int j = 0; j < chutes_dados; j++) {
 		if(chutes[j] == letra) {
 			achou = 1;
-		    	break;
+		    break;
 		}
 	}
 	return achou;
 }
 
-void desenha_forca(){ 
-	printf("Você já deu %d chutes\n", chutes_dados);
+void desenha_forca(){
+
+	int erros = chutes_errados(); // desenhando a forca com 'if ternário'.
+
+	printf(" _______	    \n");
+	printf(" |/	 |	        \n");
+	printf(" |      %c%c%c  \n", (erros>=1?'(':' '),(erros>=1?'_':' '), (erros>=1?')':' '));
+	printf(" |      %c%c%c  \n", (erros>=3?'\\':' '),(erros>=2?'|':' '), (erros>=3?'/': ' '));
+	printf(" |	 %c         \n", (erros>=2?'|':' '));
+	printf(" |	%c %c       \n", (erros>=4?'/':' '),(erros>=4?'\\':' '));
+	printf(" |			    \n");
+	printf("_|___		    \n");
+	printf("\n\n");
+
 	for(int i = 0; i < strlen(palavra_secreta); i++) {
 		if(ja_chutou(palavra_secreta[i])) {
 		        printf("%c ", palavra_secreta[i]);
-	    	} else {
+	    } else {
 			printf("_ ");
-	    	}
-    	}
-   	 printf("\n\n");
+	    }
+    }
+    printf("\n\n");
 }
 
 // Trabalhando com arquivo .txt com a lista de palavras.
@@ -70,18 +125,18 @@ void escolhe_palavra (){
 	FILE* f;
 	f = fopen("palavras.txt", "r");
 	if(f == 0) {
-        	printf("Banco de dados de palavras não disponível\n\n");
-        	exit(1);
-	}
-	
+        printf("Banco de dados de palavras não disponível\n\n");
+        exit(1);
+    }
+
 	int qtd_palavras;
 	fscanf(f, "%d", &qtd_palavras);
-	
+
 	srand(time(0));
 	int randomico = rand() % qtd_palavras;
-	
+
 	for(int i = 0; i <= randomico; i++){
-		fscanf(f, "%s", &palavra_secreta);
+		fscanf(f, "%s", palavra_secreta);
 	}
 	fclose(f);
 }
@@ -91,80 +146,50 @@ void adiciona_palavra(){
 	printf("Quer adicionar uma nova palavra ao jogo?\n");
 	scanf(" %c", &quer);
 	if(quer == 'S'){
-		char nova_palavra[20];
+		char nova_palavra[TAMANHO_PALAVRA];
 		printf("Digite a nova palavra: \n");
-		scanf("%s", &nova_palavra);
-		
-		FILE* f
+		scanf("%s", nova_palavra);
+
+		FILE* f;
 		f = fopen("palavras.txt", "r+");
 		if(f == 0) {
-        		printf("Banco de dados de palavras não disponível\n\n");
-       			exit(1);
-       		}
+        	printf("Banco de dados de palavras não disponível\n\n");
+       		exit(1);
+       	}
+
 		int qtd;
 		fscanf(f, "%d", &qtd);
 		qtd++;
-		
+
 		fseek(f, 0, SEEK_SET);
 		fprintf(f, "%d", qtd);
-		
+
 		fseek(f, 0, SEEK_END);
-		printf(f, "\n%s", nova_palavra);
-		fclose(f);		
+		fprintf(f, "\n%s", nova_palavra);
+		fclose(f);
 	}
 }
 
-// Definindo o desfecho do jogo: venceu ou perdeu?
-
-// 😄️
-int acertou(){ 
-	for(int i = 0; i < strlen(palavra_secreta); i++) {
-		if(!ja_chutou(palavra_secreta[i])){
-			return 0;
-		} 
-	}
-	return 1;
-}
-	
-// 😭️
-int enforcou(){ 
-	int erros = 0; 
-	for(int i = 0; i < chutes_dados; i++){
-		int existe = 0;
-		for(int j = 0; j < strlen(palavra_secreta); j++) {
-			if(chutes[i] == palavra_secreta[j]){
-				existe = 1;
-				break;
-			}
-		}
-		if(!existe){
-			erros++;
-		}
-	}
-	return erros >= 5;
-}
-
-// Executando o jogo:
+// 🚩️ Executando o jogo:
 
 int main() {
-			
+
 	abertura();
 	escolhe_palavra ();
-	
-	do {
-		
+
+    do {
+
 		chuta();
 		desenha_forca();
-				
+
 	} while (!acertou() && !enforcou());
-	
-	if(enforcou()){
-		printf("Perdeste e foste para a forca. Mas tente de novo!\n");
-		printf("	ヽ〳 ՞ ᗜ ՞ 〵ง  \n");
-	}
+
 	if(acertou()){
-		printf("Parabéns! Escapaste da forca!\n");
-		printf("	d–(^ ‿ ^ )z  \n");
+		printf("Parabéns! Escapaste da forca!\n\n");
+		printf("	d–(^ ‿ ^ )z  \n\n");
 		adiciona_palavra();
+	} else {
+		printf("Perdeste e foste para a forca. Mas tente de novo!\n\n");
+		printf("	ヽ〳 ՞ ᗜ ՞ 〵ง  \n\n");
 	}
 }
