4996996bfee20485ae6e0085006e46fa0a4b21a9
Eliezer <57819235+mxeliezer@users.noreply.github.com>
2020-04-26 19:33:11 +0100

    Reordenando as funções por tipo
    Reordenando funções

--- forca.h
+++ forca.h
@@ -1,8 +1,8 @@
 void abertura();
 void chuta();
 void desenha_forca();
-int ja_chutou(char letra);
 void escolhe_palavra();
+void adiciona_palavra()
+int ja_chutou(char letra);
 int acertou();
 int enforcou();
-void adiciona_palavra()
