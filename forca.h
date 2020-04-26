9a3cd059152490ccacb0cd86faa04575f28d7701
Eliezer <57819235+mxeliezer@users.noreply.github.com>
2020-04-26 21:09:03 +0100

    Upload header file
    Upload header file

--- forca.h
+++ forca.h
@@ -1,8 +1,13 @@
+#define TAMANHO_PALAVRA 20
+
 void abertura();
 void chuta();
 void desenha_forca();
 void escolhe_palavra();
-void adiciona_palavra()
 int ja_chutou(char letra);
+void adiciona_palavra();
 int acertou();
 int enforcou();
+int chutes_errados();
+int letra_existe(char letra);
+
